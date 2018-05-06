6411533a16e8e4dde7ec3e8196db8adaf0755d24
zhouhuipeng <2452412929@qq.com>
2018-05-06 14:58:36 +0800

    zhp1.0

--- app/src/main/java/com/example/spj/qxbook/MainActivity.java
+++ app/src/main/java/com/example/spj/qxbook/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.spj.qxbook;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private EditText accountEdit,passwordEdit;
+    private Button login;
+    private CheckBox rememberpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accountEdit= (EditText) findViewById(R.id.account);
+        passwordEdit= (EditText) findViewById(R.id.password);
+        rememberpassword= (CheckBox) findViewById(R.id.remember_pass);
+        login= (Button) findViewById(R.id.login);
+        preferences= PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember=preferences.getBoolean("remember_password",false);
+        if(isRemember){
+            String account =preferences.getString("account","");
+            String password=preferences.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberpassword.setChecked(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account=accountEdit.getText().toString();
+                String password=passwordEdit.getText().toString();
+                if(account.equals(account)&&password.equals(password)){
+                    editor=preferences.edit();
+                    if(rememberpassword.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else editor.clear();
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"登陆成功！",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
